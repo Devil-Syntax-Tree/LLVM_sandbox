59e680c211dd84e6c6ee5c85ab435548bcd5d6a2
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2023-09-17 22:20:49 -0500

    Revert "Kaleidoscope folder pr"

--- kaleidoscope/main.cpp
+++ kaleidoscope/main.cpp
@@ -1,23 +1,13 @@
-#include <memory>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/raw_ostream.h>
-#include "lexer/Reader.hpp"
-#include "lexer/Lexer.hpp"
-#include "lexer/Token.hpp"
+#include <string>
 
-int main()
-{
-    llvm::outs() << "Hello LLVM!!!\n\n";
+#include "Lexer.hpp"
 
-    Lexer lexer(std::make_unique<Reader>("kaleidoscope/kaleidoscope_example.txt"));
+int main(int argc, char **argv) {
+  std::string test{"1.3231"};
 
-    Token tok({ TokenType::TOK_INIT, "" });
-    while (tok.type != TokenType::TOK_EOF) {
-        tok = lexer.getToken();
-        std::cout << "got " << tok.value << " as token" << std::endl;
-    }
+  Lexer lexer {
+    []() {}
+  }
 
-    return 0;
+  return 0;
 }
--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,7 +1,6 @@
-#include <llvm/IR/IRBuilder.h>
+﻿#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include <llvm/Support/raw_ostream.h>
 
 int main() {
   llvm::outs() << "Hello LLVM!!!\n\n";
